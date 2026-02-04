8f2b728026e066ab845202b61a5e2c3465ab8176
Puja <poojas.marmat@gmail.com>
2026-02-04 22:26:11 +0530

    fixed user model path

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,4 @@
-const UserModel = require("../model/UserModel");
+const UserModel = require("../models/UserModel");
 const emailSender = require("../utility/DynamicEmailSender");
 const jwt = require("jsonwebtoken");
 const promisify = require("util").promisify;
